00faf61168f6d82ea322d8706c1031c1c9565219
vizycart <vizycart@gmail.com>
2023-11-15 18:53:20 +0530

    changed wisgi

--- crashblog/wsgi.py
+++ crashblog/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+sys.path.append(str(BASE_DIR / "crashblog"))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crashblog.settings')
 
